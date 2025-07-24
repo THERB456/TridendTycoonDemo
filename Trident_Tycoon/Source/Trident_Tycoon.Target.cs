bf1ab0703125c369212093e590958893b5473b22
LowSleepStudio <thereedbas456@gmail.com>
2025-07-24 13:21:11 +0700

    addproject

---
+++ Trident_Tycoon/Source/Trident_Tycoon.Target.cs
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Trident_TycoonTarget : TargetRules
+{
+	public Trident_TycoonTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+        
+
+        ExtraModuleNames.AddRange( new string[] { "Trident_Tycoon" } );
+	}
+}
---
+++ Trident_Tycoon/Source/Trident_Tycoon/MyBaseCPP.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+/*class TRIDENT_TYCOON_API MyBaseCPP
+{
+public:
+	MyBaseCPP();
+	~MyBaseCPP();
+};
+*/
