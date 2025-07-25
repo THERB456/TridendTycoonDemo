5aa2f18c3aa78b98704d0d57a155922685c1491f
basbas <thereedbas456@gmail.com>
2025-07-25 11:08:35 +0700

    update fix obb not yet

--- Trident_Tycoon/Build/Android/src/com/LowSleepStudio/OBBData.java
+++ Trident_Tycoon/Build/Android/src/com/LowSleepStudio/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"6", // the version of the APK that the file was uploaded against
-59304980L // the length of the file in bytes
+"10", // the version of the APK that the file was uploaded against
+59829511L // the length of the file in bytes
 )
 };
 };
